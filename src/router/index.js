dabfdbbee54aa06dc8c0605411dff0b7ffea3293
Eternallyking <2499105321@qq.com>
2022-07-15 17:58:29 +0800

    完成了页面布局的路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,26 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    redirect: '/login'
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: 'video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: 'qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: 'profile',
+        component: () => import('@/views/My')
+      }
+    ]
   },
   {
     path: '/login',
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    布局页面
+    <router-view></router-view>
+    <van-tabbar class="tab-bar" route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <i class="toutiao toutiao-shouye"></i>
+          <p>首页</p>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <i class="toutiao toutiao-shipin"></i>
+          <p>视频</p>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <i class="toutiao toutiao-wenda"></i>
+          <p>问答</p>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <i class="toutiao toutiao-wode"></i>
+          <p>我的</p>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="less">
+.tab-bar {
+  text-align: center;
+  :deep(.van-tabbar-item) {
+    margin-top: 20px;
+  }
+  :deep(.toutiao) {
+    font-size: 0.53333rem;
+  }
+  p {
+    font-size: 12px;
+  }
+}
+</style>
